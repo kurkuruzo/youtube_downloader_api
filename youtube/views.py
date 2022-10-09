98d098b6a2cd2ea30f4dc579c14707c03422e5aa
Kurkuruzo <kurkuruzo@gmail.com>
2022-10-09 18:43:48 +0300

    form creation to add video

--- youtube/views.py
+++ youtube/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+import logging
 from .models import YouTubeVideo
 from .services import add_video
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home(request):
@@ -20,8 +23,16 @@ class VideoDetails(generic.DetailView):
     template_name: str = "youtube/detail.html"
     
     
+# class AddVideo(APIView):
+#     def get(self, request, formant=None):
+#         url = request.GET.get('url')
+#         video = add_video(url)
+#         return Response(f'<a href="{video.url}">{video.name}</a> : {video.path}')
+
 class AddVideo(APIView):
-    def get(self, request, formant=None):
-        url = request.GET.get('url')
-        video = add_video(url)
-        return Response(f'<a href="{video.url}">{video.name}</a> : {video.path}')
+    def get(self, request, format=None):
+        logger.info(request)
+        if request.method == "POST":
+            url = request.data.get('url')
+            video = add_video(url)
+            return Response(f'<a href="{video.url}">{video.name}</a> : {video.path}')
