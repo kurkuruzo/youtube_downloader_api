1c7a13322caea91cee5ea90a4bdaf40c6e2fbf2b
kurkuruzo <kurkuruzo@gmail.com>
2022-10-25 20:06:04 +0600

    modifies endpoints

--- youtube/urls.py
+++ youtube/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("videos", views.VideosList.as_view(), name="videos"),
-    path("videos/details/<str:pk>", views.VideoDetails.as_view(), name="video-details"),
-    path("videos/add-video-form", views.AddVideoForm.as_view(), name="add-video-form"),
+    path("videos/<str:pk>", views.VideoDetails.as_view(), name="video-details"),
+    path("videos/", views.AddVideoForm.as_view(), name="add-video-form"),
     path("videos/url", views.GetVideoByUrl.as_view(), name="get-video-by-url"),
     path("videos/confirmation", views.confirmation, name="confirmation"),
     path("api/videos", views.AddVideo.as_view(), name="add-video"),
