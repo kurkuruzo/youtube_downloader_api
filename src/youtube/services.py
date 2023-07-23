9b12763ea575ed6c65cf50e0d94f6c964686449b
kurkuruzo <kurkuruzo@gmail.com>
2023-07-23 21:41:47 +0600

    Change from pytube to yt_dlp

--- src/youtube/services.py
+++ src/youtube/services.py
@@ -4,6 +4,7 @@ from turtle import title
 import pytube
 from typing import Optional
 from django.utils import timezone
+from yt_dlp import YoutubeDL
 from .models import YouTubeVideo
 from .tasks import download_video
 
@@ -19,44 +20,39 @@ def add_video(url: str, chat_id: Optional[int] = None, message_id: Optional[int]
     logger.info(f"STARTING ASYNC TASK FOR VIDEO {url}")
     video_in_db = _video_in_db(url)
     logger.info(f"{video_in_db=}")
-    yt_downloader = _get_downloader(url=url)
-    if video_in_db:
-        if not video_in_db.filesize_OK:
-            video = video_in_db
+    # yt_downloader = _get_downloader(url=url)
+    with YoutubeDL() as ydl:
+        if video_in_db:
+            if not video_in_db.filesize_OK:
+                video = video_in_db
+            else:
+                return (video_in_db.id, "")
         else:
-            return (video_in_db.id, "")
-    else:
-        video = _create_video_obj(url=url, downloader=yt_downloader)
-    return _download_video(url=url, downloader=yt_downloader, video=video, chat_id=chat_id, message_id=message_id)
+            video = _create_video_obj(url=url, downloader=ydl)
+        download_status = _download_video(url=url, downloader=ydl, video=video, chat_id=chat_id, message_id=message_id)
+    return download_status
 
-def _download_video(url: str, downloader: pytube.YouTube, video: YouTubeVideo, chat_id: int, message_id: int):
-    try:
-        stream = downloader.streams.get_highest_resolution()
-    except Exception as e:
-        logger.exception(e)
-        raise YouTubeError(e)
-    else:
-        if not YouTubeVideo.objects.filter(id=str(video.id)):
-            video.path = stream.default_filename
-            video.filesize = stream.filesize
-            video.save()
-        download_task = download_video.delay(stream, video, chat_id, message_id)
-        logger.info(f"{download_task=}")
-        return (video.id, download_task.id)
+def _download_video(url: str, downloader: YoutubeDL, video: YouTubeVideo, chat_id: int, message_id: int):
+    download_task = download_video.delay(downloader, video, chat_id, message_id)
+    logger.info(f"{download_task=}")
+    return (video.id, download_task.id)
 
-def _create_video_obj(url: str, downloader: pytube.YouTube) -> YouTubeVideo:
+def _create_video_obj(url: str, downloader: YoutubeDL) -> YouTubeVideo:
     try:
-        video_title = downloader.title
+        info = downloader.extract_info(url, download=False)
+        sanitized_info = downloader.sanitize_info(info)
     except Exception as e:
-        logger.exception(e)
-        video_title = "Не удалось получить название видео"
-    video_obj = YouTubeVideo(
-        name=video_title,
-        description=downloader.description,
+        raise YouTubeError("Возникла ошибка при получении информации о видео. Объект для скачивания не создан.")
+    if not sanitized_info:
+        raise YouTubeError("Не удалось получить информацию о видео. Объект для скачивания не создан.")
+    video_obj = YouTubeVideo.objects.create(
+        name=sanitized_info.get("title"),
+        description=sanitized_info.get("description"),
         url=url,
-        length=downloader.length,
+        length=sanitized_info.get("duration"),
         date_added=timezone.now(),
-        thumbnail=downloader.thumbnail_url,
+        thumbnail=sanitized_info.get("thumbnail"),
+        path=downloader.prepare_filename(sanitized_info)
     )
     logger.info(f"{video_obj.__dict__=}")
     return video_obj
@@ -69,14 +65,16 @@ def _video_in_db(url: str) -> Optional[YouTubeVideo]:
     return YouTubeVideo.objects.filter(url=url).first()
 
 
-def _get_downloader(url: str) -> pytube.YouTube:
-    yt_downloader = pytube.YouTube(
-        url,
-        on_complete_callback=_download_completed,
-        on_progress_callback=_download_progress,
-    )
-    logger.info(yt_downloader)
-    return yt_downloader
+# def _get_downloader(url: str) -> :
+    # yt_downloader = pytube.YouTube(
+    #     url,
+    #     on_complete_callback=_download_completed,
+    #     on_progress_callback=_download_progress,
+    # )
+    # logger.info(yt_downloader)
+    # return yt_downloader
+    
+
 
 
 def _download_completed(stream: pytube.Stream, file_path: str = None) -> None:
--- src/youtube/tasks.py
+++ src/youtube/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 import json
 import logging
 import pytube
+import yt_dlp
 
 from .models import YouTubeVideo
 from api.settings import DOWNLOAD_PATH
@@ -10,12 +11,14 @@ from .producer import send_download_confirmation
 logger = logging.getLogger(__name__)   
 
 @shared_task(serializer='pickle')
-def download_video(yt_stream: pytube.Stream, yt_video: YouTubeVideo, chat_id: int, message_id: int, download_path: str=DOWNLOAD_PATH, skip_existing=True):
-    logger.info(f"Downloading video: {yt_stream.title}")
-    file_path = yt_stream.download(output_path=download_path, skip_existing=skip_existing)
-    logger.info(f"Video {yt_stream.title} saved to {file_path}")
+def download_video(downloader: yt_dlp.YoutubeDL, yt_video: YouTubeVideo, chat_id: int, message_id: int, download_path: str=DOWNLOAD_PATH, skip_existing=True):
+    logger.info(f"Downloading video: {yt_video.name}")
+    download_status = downloader.download([yt_video.url])
+    if not download_status == 0:
+        raise Exception("Ошибка при загрузке видео")
+    logger.info(f"Video {yt_video.name} saved to {yt_video.path}")
     logger.info(f"{chat_id=}")
     logger.info(f"{message_id=}")
     if chat_id:
         send_download_confirmation(json.dumps({'yt_video_id': str(yt_video.id), "chat_id": chat_id, "message_id": message_id}))
-    return file_path
+    return yt_video.path
